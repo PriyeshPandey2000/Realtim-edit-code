115039d8e24e467333e78e00d559b85c89ada9f0
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-05-22 22:46:40 +0530

    .

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const ACTIONS = require("./Actions");
 const path=require("path");
 
 
-const __dirname1=path.resolve();
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname1,"/client/build")));
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-  })
-}
+// const __dirname1=path.resolve();
+// if(process.env.NODE_ENV==="production"){
+//   app.use(express.static(path.join(__dirname1,"/client/build")));
+//   app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+//   })
+// }
 
 
 
