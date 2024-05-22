7735415ec7615509275401426fc3a41db0f30966
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-05-23 01:16:43 +0530

    .

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -7,6 +7,7 @@ function Client({username}) {
     <div className="d-flex align-items-center mb-3">
       <Avatar name={username.toString()} size={50} round="14px" className="mr-3" />
       <span className='mx-2'>{username.toString()}</span>
+      
     </div>
   );
 }
