ef8cb0b7b80e5d11f644bca8ff104d3f4dddc300
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-05-22 17:15:08 +0530

    .

--- server/index.js
+++ server/index.js
@@ -3,6 +3,18 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const ACTIONS = require("./Actions");
+const path=require("path");
+
+
+const __dirname1=path.resolve();
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname1,"/client/build")));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  })
+}
+
+
 
 const server = http.createServer(app);
 
